6e63adaf6b7e773668a87b701840168fc82d2dda
Lancia144 <danielnduatie@gmail.com>
2026-02-10 22:17:26 -0800

    first commit

---
+++ python_basics/Challenge 1.py
@@ -0,0 +1,8 @@
+university = "JKUAT"
+grade = "B+"
+favourite_course = "automative engineering"
+cluster_points = 71
+print (f"My favourite university is {university} ")
+print (f"I got a {grade}")
+print (f"I pursue {favourite_course}")
+print (f"I have {cluster_points}")
---
+++ python_basics/hello.py
@@ -0,0 +1,11 @@
+# this is a comment
+print ("Hello from Dan")
+print ("i am from kiambu")
+# city / town
+print ("i like formula 1,walking and breathing")
+# my hobbies
+age = 23 
+height = 1.43
+weight = 76
+print (f"i am also {height} metres")
+print (f"i {weight} kg")
