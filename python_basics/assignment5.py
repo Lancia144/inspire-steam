d17aed716bd71aca8e201901e5651787991c9dbc
Lancia144 <danielnduatie@gmail.com>
2026-02-16 11:04:58 +0300

    assignment_5

--- python_basics/assignment5.py
+++ python_basics/assignment5.py
@@ -2,12 +2,23 @@
 # date : 16/02/2026
 
 salary = int(input("Enter your gross salary"))
-if  salary < 50000:
+if  salary <= 50000:
     tax = (2.5 * salary) /100
     net_salary = salary - tax
-print(f"Gross salary = {salary}")
-print(f"Net salary = {net_salary}")
-print(f"Tax = {tax}")
+    print(f"Gross salary = {salary}")
+    print(f"Net salary = {net_salary}")
+    print(f"Tax = {tax}")
 
-elif salary > 50000:
+elif salary >50000 <100000:
     tax = (4.5 * salary) /100
+    net_salary = salary - tax
+    print(f"Gross salary = {salary}")
+    print(f"Net salary = {net_salary}")
+    print(f"Tax = {tax}")
+    
+elif salary >100000:
+    tax = (4.5 * salary) /100
+    net_salary = salary - tax
+    print(f"Gross salary = {salary}")
+    print(f"Net salary = {net_salary}")
+    print(f"Tax = {tax}")
